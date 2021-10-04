9a3da04403577370bf35414feff673ee38ebe0eb
Nate Stemen <nate.stemen@overleaf.com>
2021-10-04 13:21:46 -0400

    formatter wasn't configured properly

--- gr/contours.py
+++ gr/contours.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "sans-serif",
-    "font.sans-serif": ["Helvetica"]
-})
+plt.rcParams.update(
+    {"text.usetex": True, "font.family": "sans-serif", "font.sans-serif": ["Helvetica"]}
+)
 
 num_points = 101
 
 x, y = np.linspace(-5, 5, num_points), np.linspace(-5, 5, num_points)
 
+
 def mink_dist(x1, x2):
     return -pow(x1[0] - x2[0], 2) + pow(x1[1] - x2[1], 2)
+
 
 dists = np.zeros((num_points, num_points))
 for i, xv in enumerate(x):
     for j, yv in enumerate(y):
-        dists[i, j] = mink_dist((0,0), (xv, yv))
+        dists[i, j] = mink_dist((0, 0), (xv, yv))
 
 plt.pcolormesh(x, y, dists, shading="auto", cmap="magma")
 plt.colorbar()
